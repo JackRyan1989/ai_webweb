e7eae1cf8b7b25f2211621aeee7b617ebbd8bb1c
JackRyan1989 <jack.jackryan@protonmail.com>
2025-05-23 20:55:50 -0400

    Get rid of annoying confirmation

--- src/app/components/sessionsDisplay.tsx
+++ src/app/components/sessionsDisplay.tsx
@@ -1,13 +1,10 @@
 "use client";
 
 import { ReactNode, useEffect, useRef, useState } from "react";
-import { getFirstConversationForASession } from '../db/query'
+import { getFirstConversationForASession } from "../db/query";
 
 const sessionHandler = (id: number, sessionSetter: (id: number) => void) => {
-    if (confirm(`Set Session ID to ${id}?`)) {
-        sessionSetter(id);
-    }
-    return
+    sessionSetter(id);
 };
 
 export default function SessionDisplay(
@@ -17,35 +14,48 @@ export default function SessionDisplay(
         session: number | null;
     },
 ): ReactNode | Array<ReactNode> {
-
-    const [conversations, setConversations] = useState<{content: string, sessionId: number}[]>([]);
-    const interMedArray = useRef<{content: string, sessionId: number}[]>([])
+    const [conversations, setConversations] = useState<
+        { content: string; sessionId: number }[]
+    >([]);
+    const interMedArray = useRef<{ content: string; sessionId: number }[]>([]);
 
     useEffect(() => {
-            sessions.forEach(async ({id}) => {
-                const {status, payload} = await getFirstConversationForASession(id)
-                if (status == 'success' && payload?.content.length !== undefined) {
-                    interMedArray.current = [...interMedArray.current, {content: payload.content, sessionId: payload.sessionId}]
-                    setConversations([...interMedArray.current, {content: payload.content, sessionId: payload.sessionId}])
-                } else {
-                    return
-                }
-            })
-
-    }, [sessions])
+        sessions.forEach(async ({ id }) => {
+            const { status, payload } = await getFirstConversationForASession(
+                id,
+            );
+            if (status == "success" && payload?.content.length !== undefined) {
+                interMedArray.current = [...interMedArray.current, {
+                    content: payload.content,
+                    sessionId: payload.sessionId,
+                }];
+                setConversations([...interMedArray.current, {
+                    content: payload.content,
+                    sessionId: payload.sessionId,
+                }]);
+            } else {
+                return;
+            }
+        });
+    }, [sessions]);
 
     return (
         sessions.map((sesh: { id: number }, index: number) => {
-            const currConvo = conversations.filter((convo) => convo.sessionId === sesh.id)[0]
+            const currConvo = conversations.filter((convo) =>
+                convo.sessionId === sesh.id
+            )[0];
             return (
-            <button
-                onClick={() => sessionHandler(sesh.id, sessionSetter)}
-                className={`min-w-min text-sm border-solid border-black outline p-[.5rem] m-[.5rem] rounded-xs ${session === sesh.id ? 'bg-lime-200' : 'bg-transparent'}`}
-                key={sesh.id}
-                id={String(sesh.id)}
-            >
-                {currConvo?.content ?? <span>Loading session...</span>}
-            </button>
-        )})
+                <button
+                    onClick={() => sessionHandler(sesh.id, sessionSetter)}
+                    className={`min-w-min text-sm border-solid border-black outline p-[.5rem] m-[.5rem] rounded-xs ${
+                        session === sesh.id ? "bg-lime-200" : "bg-transparent"
+                    }`}
+                    key={sesh.id}
+                    id={String(sesh.id)}
+                >
+                    {currConvo?.content ?? <span>Loading session...</span>}
+                </button>
+            );
+        })
     );
 }
