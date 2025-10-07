09a2e3a68c309173261b62eb9536d91b6b5151a8
JackRyan1989 <jack.jackryan@protonmail.com>
2025-10-07 10:36:01 -0400

    Web search handlers

---
+++ src/app/brains/tools/webSearch.ts
@@ -0,0 +1,36 @@
+import search from '@/app/serp_api/webSearch'
+
+export const getHtmlRes = async (args: { query: string }) => {
+    try {
+        const response = await search(args.query);
+        const firstRes = response.organic_results[0];
+        const link = firstRes.link;
+        try {
+            const res = await fetch(link);
+            const body = await res.text();
+            const parser = new DOMParser();
+            const html = parser.parseFromString(body, 'text/html');
+            return html.querySelector('body')?.innerHTML;
+        } catch (err) {
+            throw err
+        }
+    } catch (err) {
+        throw err
+    }
+}
+
+// Tool Definition:
+export const conductWebSearchTool = {
+    type: 'function',
+    function: {
+        name: 'getHtmlRes',
+        description: "Search for resources on the internet for a specific topic.",
+        parameters: {
+            type: 'object',
+            required: ['query'],
+            properties: {
+                query: { type: "string", description: "The term to search for" }
+            }
+        }
+    }
+}
---
+++ src/app/serp_api/webSearch.ts
@@ -0,0 +1,19 @@
+'use server'
+import { getJson, BaseResponse, EngineParameters } from "serpapi";
+import '../../../envConfig.ts'
+
+const search = async (query: string, engine = "duckduckgo"): Promise<BaseResponse | EngineParameters> => {
+    const response = await getJson({
+        engine: engine,
+        api_key: process.env.SERPAPI_KEY,
+        q: query,
+        kl: "us-en",
+    });
+
+    console.log(response);
+
+    return response;
+}
+
+export default search;
+
