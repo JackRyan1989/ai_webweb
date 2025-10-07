83ea0d8eee80803807acb87a97d4d47bddeb1a33
JackRyan1989 <jack.jackryan@protonmail.com>
2025-10-07 10:36:26 -0400

    Add new tool

--- src/app/brains/ollama.ts
+++ src/app/brains/ollama.ts
@@ -1,16 +1,17 @@
 "use server";
 import ollama, { ChatResponse } from "ollama";
-import type { ListResponse } from "ollama";
+import type { ListResponse, Tool } from "ollama";
 
 export interface ErrorObj {
   errStatus: string,
   message: string,
 }
 
-export async function chat(content: { role: string; content: string; }[], model: string): Promise<ChatResponse> {
+export async function chat(content: { role: string; content: string; }[], model: string, tools: Tool[]): Promise<ChatResponse> {
   return await ollama.chat({
     model,
     messages: content,
+    tools
   });
 }
 
